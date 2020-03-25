cbe684204b5f223cda5f08636128924bd4883211
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 10:47:40 -0600

    Added method to return hash with products broken down by category

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -50,6 +50,10 @@ class ShoppingCart
   end
 
   def product_breakdown
-
+    product_breakdown = {}
+      @products.each do |product|
+        product_breakdown[product.category] = products_by_category(product.category)
+      end
+    product_breakdown
   end
 end
