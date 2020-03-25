5e9f679d14a426abba6abbe41b24857cff5aedf6
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 09:12:42 -0600

    Fix errors in Product class

--- lib/product.rb
+++ lib/product.rb
@@ -1,13 +1,14 @@
-class Prodct
+class Product
+  attr_reader :category, :name, :unit_price, :quantity
 
   def initialize(category, name, unit_price, quantity)
     @category = category
-    @name = name_parameter
-    @unit_price = 3.70
+    @name = name
+    @unit_price = unit_price
     @quantity = quantity
   end
 
   def total_price
-    @unit_price * @quantity
+    @unit_price * (@quantity.to_i)
   end
 end
