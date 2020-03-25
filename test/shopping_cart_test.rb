1297f94078c4328fb1c93be1e79e16740ec58062
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 09:48:06 -0600

    Add test for total_number_of_products method

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -20,7 +20,6 @@ class ShoppingCartTest < Minitest::Test
   end
 
   def test_it_can_add_product
-    skip
     cart = ShoppingCart.new("King Soopers", "30items")
     product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
     product2 = Product.new(:meat, 'chicken', 4.50, '2')
@@ -28,5 +27,32 @@ class ShoppingCartTest < Minitest::Test
     cart.add_product(product2)
 
     assert_equal [product1, product2], cart.products
+  end
+
+  def test_it_can_count_total_number_of_items
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+
+    assert_equal 13, cart.total_number_of_products
+  end
+
+  def test_it_can_detect_if_full
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    assert_equal false, cart.is_full?
+
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product4)
+    assert_equal true, cart.is_full?
   end
 end
