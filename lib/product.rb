69489adbbed13491454cbc4e575e403066582193
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 09:22:24 -0600

    Create method for hoard and is_hoarded?

--- lib/product.rb
+++ lib/product.rb
@@ -6,9 +6,19 @@ class Product
     @name = name
     @unit_price = unit_price
     @quantity = quantity
+    @hoard = false
   end
 
   def total_price
     @unit_price * (@quantity.to_i)
+  end
+
+  def hoard
+    @quantity = @quantity.to_i * 3
+    @hoard = true
+  end
+
+  def is_hoarded?
+    @hoard
   end
 end
