4ec6f95dab6858fcaab22c513ba79ad6b1f74a27
stellakunzang <stella.k.bonnie@gmail.com>
2020-03-25 09:47:44 -0600

    Add method to calculate total number of products

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,9 +1,37 @@
 class ShoppingCart
-  attr_reader :name, :capacity, :products
+  attr_reader :name, :capacity, :products, :total_number_of_products
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity.delete('items')
     @products = []
+    @total_number_of_products = 0
   end
-end 
+
+  def add_product(product)
+    @products << product
+  end
+
+  def total_number_of_products
+    @products.each do |product|
+      @total_number_of_products += product.quantity.to_i
+    end
+    @total_number_of_products
+  end
+
+  def is_full?
+    if total_number_of_products >= @capacity.to_i
+      true
+    else
+      false
+    end
+  end
+
+  def products_by_category
+
+  end
+
+  def details
+
+  end
+end
